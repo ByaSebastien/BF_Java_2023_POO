f2f5f4528b849a9a7bf5d0f8674d7e7f9b0007dd
Sebastien <byasebastien@hotmail.com>
2023-07-17 09:37:38 +0200

    demo collection

--- src/main/java/be/bstorm/demos/demoCollection/Main.java
+++ src/main/java/be/bstorm/demos/demoCollection/Main.java
@@ -2,7 +2,7 @@ package be.bstorm.demos.demoCollection;
 
 import be.bstorm.demos.basicClass.Person;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
@@ -26,16 +26,101 @@ public class Main {
 //            }
 //        }
 
-        ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(42);
+//        ArrayList<String> names = new ArrayList<>();
+//
+//        names.add("Pierre");
+//        names.add("Pol");
+//        names.add("Jacques");
+//
+////        names.addAll(List.of("Pierre","Pol","Jacques"));
+//
+//        names.remove("Pol");
+////        names.remove(1);
+//
+////        System.out.println(names.get(0));
+//
+//        names.add(1,"Spicy");
+//
+//        for (String s : names){
+//
+//            System.out.println(s);
+//        }
 
-        ArrayList<String> names = new ArrayList<>();
-        names.add("Pierre");
-        names.add("Pol");
-        names.add("Jacque");
+//        HashMap<Integer,String> communes = new HashMap<>();
+//
+//        communes.put(4000,"Liège");
+//        communes.put(1000,"Bruxelle");
+//        communes.put(4347,"Fexhe");
+//
+//        System.out.println(communes.get(4000));
+//
+//        for (int cp : communes.keySet()){
+//
+//            System.out.println(cp);
+//            System.out.println(communes.get(cp));
+//            System.out.println("___________________________________");
+//        }
+//
+//        for (String s : communes.values()){
+//            System.out.println(s);
+//            System.out.println("___________________________________");
+//        }
+//
+//        for (Map.Entry<Integer,String> entry : communes.entrySet()){
+//
+//            System.out.println(entry.getKey());
+//            System.out.println(entry.getValue());
+//            System.out.println("___________________________________");
+//        }
 
-        ArrayList<Person> people = new ArrayList<>();
-        people.add(new Person("Cesar","Jule"));
+        Set<Person> people = new HashSet<>();
+        people.add(new Person("Lee","Bruce"));
+
+        HashMap<Integer,Commune> communes = new HashMap<>();
+
+        Commune c = new Commune(4000,"Liège",100_000);
+
+        communes.put(c.getCodePostal(),c);
+
+        System.out.println(communes.get(4000).getNom());
+
+//        ArrayList<Commune> communes1 = new ArrayList<>();
+//        communes1.add(c);
+//
+//        communes1.get(0);
+
+        HashMap<String,Person> persons = new HashMap<>();
+
+        Person p = new Person("Lee","Bruce");
+
+        persons.put(p.getNom(),p);
+
+        System.out.println(persons.get("Lee").getPrenom());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
